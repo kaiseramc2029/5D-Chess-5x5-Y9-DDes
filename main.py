2d5e7f75353ed54506ddcbdc13d034904fcb6d5e
kaiseramc2029 <kaiseramc2029@student.cis.edu.hk>
2025-03-03 22:07:12 +0800

    Update main.py
    Signed-off-by: kaiseramc2029 <kaiseramc2029@student.cis.edu.hk>

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # 5D-Chess-5x5-Y9-DDes
 # Vincent Chen, Kaiser Choi - Year of 2029. DDes Semester 2 Final Project - 5D 5x5 Chess
+import chess
+import math
+import py-game
 print("hello world")
 i = "hello world"
 print(i)
